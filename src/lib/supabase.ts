1a0a7ab3d32dc5422edaf2db8fb7a4b1c8806333
HeiST GameR <macs271193@gmail.com>
2025-09-27 10:23:00 +0000

    Error: [ Server ] Error fetching visitor count: "FetchError: request to

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js'
 import fetch from 'cross-fetch';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = "https://zuvslodnigwuqfciqgvs.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1dnNsb2RuaWd3dXFmY2lxZ3ZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg5NjU1OTQsImV4cCI6MjA3NDU0MTU5NH0.2UMbPvOjtsgVDL-NDRdZTSAG9aesXbSrYpn_ZgVSnuU"
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase URL or anon key')
