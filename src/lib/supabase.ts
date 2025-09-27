2de679e4cab4f2e20ef10e8fc45fe5e75d4b2e68
HeiST GameR <macs271193@gmail.com>
2025-09-27 11:06:17 +0000

    CREATE POLICY "Allow server-side uploads" ON storage.objects FOR INSERT

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js'
+import { fetch as crossFetch } from 'cross-fetch';
 
 const supabaseUrl = "https://zuvslodnigwuqfciqgvs.supabase.co"
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1dnNsb2RuaWd3dXFmY2lxZ3ZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg5NjU1OTQsImV4cCI6MjA3NDU0MTU5NH0.2UMbPvOjtsgVDL-NDRdZTSAG9aesXbSrYpn_ZgVSnuU"
